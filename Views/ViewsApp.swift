a682a0acee14d2aa1d12467e65bb0faa6c836bbb
kirabin <kirabin.one@gmail.com>
2021-12-12 12:45:07 +0300

    Initial Commit

---
+++ Views/ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsApp.swift
+//  Views
+//
+//  Created by kirabin on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
